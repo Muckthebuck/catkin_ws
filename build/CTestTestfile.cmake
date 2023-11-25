9e332734802a2afaaf8618a589835e124b979fd4
Mukul Chodhary <70460358+Muckthebuck@users.noreply.github.com>
2023-11-26 00:45:41 +0900

    ros-melodic with lego-loam

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/mukul/catkin_ws/src
+# Build directory: /home/mukul/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("LeGO-LOAM-BOR/cloud_msgs")
+subdirs("LeGO-LOAM-BOR/LeGO-LOAM")
---
+++ build/LeGO-LOAM-BOR/LeGO-LOAM/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mukul/catkin_ws/src/LeGO-LOAM-BOR/LeGO-LOAM
+# Build directory: /home/mukul/catkin_ws/build/LeGO-LOAM-BOR/LeGO-LOAM
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
